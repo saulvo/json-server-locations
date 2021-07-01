3fd527b833044c92be3e309f592b058d5950e569
sonvt-fe <vtson23597@gmail.com>
2021-07-01 15:31:57 +0700

    fixed

--- main.js
+++ main.js
@@ -11,17 +11,6 @@ server.get('/echo', (req, res) => {
   res.jsonp(req.query)
 })
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-  }
-  // Continue to JSON Server router
-  next()
-})
-
 // Use default router
 server.use(router)
 server.listen(3000, () => {
